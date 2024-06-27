3c209d4e900444342932dbd3d556e599f5f3bf61
Sami-AL-njadat <sami.alnajadat@gmail.com>
2024-06-27 19:00:38 +0300

    s

--- resources/views/page/authraization/login.blade.php
+++ resources/views/page/authraization/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Basic Log In | Front - Admin &amp; Dashboard Template</title>
+    <title>idex - Admin &amp; Dashboard </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Favicon -->
